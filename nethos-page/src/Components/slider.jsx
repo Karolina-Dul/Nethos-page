4fb3b99ada37a3479c14a36d040b983c6aec0f9a
KarolinaDuljas <duljas.karolina@gmail.com>
2024-08-26 21:46:52 +0200

    final page

--- nethos-page/src/Components/slider.jsx
+++ nethos-page/src/Components/slider.jsx
@@ -1,7 +1,5 @@
-import React, { useState, useEffect } from "react";
 import "./LogosSlider.css";
 import allegro from "../Images/allegro.png";
-// import bookbeat from "../Images/bookbeat.png";
 import grycan from "../Images/grycan.png";
 import legimi from "../Images/legimi.png";
 import lisek from "../Images/lisek.png";
@@ -10,7 +8,7 @@ import modivo from "../Images/modivo.png";
 import rossmann from "../Images/rossmann.png";
 
 export const Slider = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+  // const [currentIndex, setCurrentIndex] = useState(0);
   const logos = [
     { id: 1, src: allegro, alt: "allegro 1" },
     { id: 2, src: rossmann, alt: "allegro 2" },
@@ -21,34 +19,15 @@ export const Slider = () => {
     { id: 7, src: modivo, alt: "allegro 7" },
   ];
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % logos.length);
-    }, 3000);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  const nextSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % (logos.length - 4));
-  };
-
-  const prevSlide = () => {
-    setCurrentIndex(
-      (prevIndex) => (prevIndex - 1 + (logos.length - 4)) % (logos.length - 4)
-    );
-  };
-
   return (
     <div className="slider-container">
-      <button onClick={prevSlide} className="slider-button">
-        ❮
-      </button>
       <div className="slider">
-        <div
-          className="slider-content"
-          style={{ transform: `translateX(-${currentIndex * 20}%)` }}
-        >
+        <div className="slider-content">
+          {logos.map((logo) => (
+            <div key={logo.id} className="logo-wrapper">
+              <img src={logo.src} alt={logo.alt} className="logo" />
+            </div>
+          ))}
           {logos.map((logo) => (
             <div key={logo.id} className="logo-wrapper">
               <img src={logo.src} alt={logo.alt} className="logo" />
@@ -56,9 +35,6 @@ export const Slider = () => {
           ))}
         </div>
       </div>
-      <button onClick={nextSlide} className="slider-button">
-        ❯
-      </button>
     </div>
   );
 };
